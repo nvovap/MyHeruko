36963f0369b31da190964e543fa0453838285a70
Vladimir Nevinniy <nvovap@gmail.com>
2016-03-23 22:13:26 +0200

    edit package.json five

--- app.js
+++ app.js
@@ -3,48 +3,9 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , mongoose = require('mongoose')
-  , http = require('http')
-  , path = require('path')
-    , todoModel = require('./models/todo')
-    ,todoRoute = require('./routes/todo');
-
-var app = express();
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-var uriString =
-    process.env.MONGOLAB_URI ||
-        process.env.MONGOHQ_URL ||
-        'mongodb://localhost/HelloMongoose';
-
-mongoose.connect('mongodb://gregg:test@ds057954.mongolab.com:57954/alamofire-db');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-   console.log('Successfully mongodb is connected');
-});
-
-app.get('/todo',todoRoute.index);
-app.get('/todo/:id',todoRoute.findById);
-app.put('/todo/:id',todoRoute.update);
-app.delete('/todo/:id',todoRoute.delete)
-app.post('/todo',todoRoute.newTodo);
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+var express = require('express'); // 1
+var app = express(); // 2
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) { // 3
+        res.send('hello world'); //4
+        });
